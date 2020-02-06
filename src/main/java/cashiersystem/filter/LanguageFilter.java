cd65d070c612630a2724d4eb195a37e23a508ec8
StrafeUp <bogdan22693@gmail.com>
2020-02-06 14:32:00 +0200

    Added LanguageFilter

---
+++ src/main/java/cashiersystem/filter/LanguageFilter.java
@@ -0,0 +1,27 @@
+package cashiersystem.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class LanguageFilter implements Filter {
+
+    public static final String DEFAULT_LANGUAGE = "en";
+    public static final String SESSION_LOCALE_PARAMETER = "lang";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        if (req.getParameter(SESSION_LOCALE_PARAMETER) != null) {
+            req.getSession().setAttribute(SESSION_LOCALE_PARAMETER, req.getParameter(SESSION_LOCALE_PARAMETER));
+        } else {
+            req.getSession(true).setAttribute(SESSION_LOCALE_PARAMETER, DEFAULT_LANGUAGE);
+        }
+        chain.doFilter(req, response);
+    }
+}
